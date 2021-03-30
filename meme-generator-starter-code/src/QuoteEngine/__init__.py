e55aade3e6c13e7c5c5d09af181bf84f012bcf39
Panagiotis Vardanis <37624791+pvardanis@users.noreply.github.com>
2021-03-30 14:14:31 +0200

    Delete meme-generator-starter-code directory

--- meme-generator-starter-code/src/QuoteEngine/__init__.py
+++
@@ -1,6 +0,0 @@
-from .docx_ingestor import DocxIngestor
-from .pdf_ingestor import PDFIngestor
-from .csv_ingestor import CSVIngestor
-from .txt_ingestor import TxtIngestor
-from .ingestor import Ingestor
-from .quote_model import QuoteModel
